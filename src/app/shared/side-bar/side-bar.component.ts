ecc1fa3faa8f44ca9a167483c0ba11a73b42092f
SnowFall98 <yordymora98@gmail.com>
2021-12-28 17:32:41 -0500

    Funcion LogOut

--- src/app/shared/side-bar/side-bar.component.ts
+++ src/app/shared/side-bar/side-bar.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-side-bar',
@@ -7,9 +8,21 @@ import { Component, OnInit } from '@angular/core';
 })
 export class SideBarComponent implements OnInit {
 
-  constructor() { }
+  constructor(private router: Router) { }
 
   ngOnInit(): void {
+  }
+
+  /*=============================================
+	Función de salida del sistema
+	=============================================*/
+
+  logout(){
+
+    localStorage.removeItem('token');
+    localStorage.removeItem('refreshToken');
+    this.router.navigateByUrl("/login");
+
   }
 
 }
