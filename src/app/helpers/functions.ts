c86b66e2a6e0be36f0f12c26ecc6c7bde5ca8768
SnowFall98 <yordymora98@gmail.com>
2021-12-28 16:48:18 -0500

    Archivo de re uso de Funciones

---
+++ src/app/helpers/functions.ts
@@ -0,0 +1,26 @@
+import { FormGroup } from  '@angular/forms';
+
+export class functions{
+
+	/*=============================================
+	Función para validar campos del formulario
+	=============================================*/
+
+	static invalidField(field:string, f:FormGroup, formSubmitted:boolean):boolean{
+
+		if(formSubmitted && f.controls[field].invalid){
+
+		   	return true;
+
+		}else{
+
+			return false;
+		}
+
+	}
+	
+
+}
+
+
+	
