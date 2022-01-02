0c6b47390027be55a78a572b39d089527c86bfb3
SnowFall98 <yordymora98@gmail.com>
2022-01-02 14:52:38 -0500

    Importación de los Módulos de Angular Material

--- src/app/pages/main-page/users/users.module.ts
+++ src/app/pages/main-page/users/users.module.ts
@@ -7,11 +7,19 @@ import { UsersRoutingModule } from './users-routing.module';
 //componente
 import { UsersComponent } from './users.component';
 
+//Angular Material
+import { MatTableModule } from '@angular/material/table';
+import { MatPaginatorModule } from '@angular/material/paginator';
+import { MatSortModule } from '@angular/material/sort';
+
 @NgModule({
   declarations: [UsersComponent],
   imports: [
     CommonModule,
-    UsersRoutingModule
+    UsersRoutingModule,
+    MatTableModule,
+    MatPaginatorModule,
+    MatSortModule
   ]
 })
 export class UsersModule { }
