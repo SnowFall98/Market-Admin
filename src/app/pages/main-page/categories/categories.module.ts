a62e3c03962a7908f4cf63d8265477dd2999210f
SnowFall98 <yordymora98@gmail.com>
2022-01-11 19:28:42 -0500

    Configuración del Modulo de Categories

--- src/app/pages/main-page/categories/categories.module.ts
+++ src/app/pages/main-page/categories/categories.module.ts
@@ -7,11 +7,25 @@ import { CategoriesRoutingModule } from './categories-routing.module';
 //componente
 import { CategoriesComponent } from './categories.component';
 
+//Angular Material
+import { MatTableModule } from '@angular/material/table';
+import { MatPaginatorModule } from '@angular/material/paginator';
+import { MatSortModule } from '@angular/material/sort';
+import {MatDialogModule} from '@angular/material/dialog';
+
+//pipes
+import { PipesModule } from '../../../pipes/pipes.module';
+
 @NgModule({
   declarations: [CategoriesComponent],
   imports: [
     CommonModule,
-    CategoriesRoutingModule
+    CategoriesRoutingModule,
+    MatTableModule,
+    MatPaginatorModule,
+    MatSortModule,
+    PipesModule,
+    MatDialogModule
   ]
 })
 export class CategoriesModule { }
