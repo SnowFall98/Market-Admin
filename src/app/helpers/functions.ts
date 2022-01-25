501fd84e875d64153cad01cd25b50a726454e677
SnowFall98 <yordymora98@gmail.com>
2022-01-24 19:12:07 -0500

    Nuevas funciones globales

--- src/app/helpers/functions.ts
+++ src/app/helpers/functions.ts
@@ -1,4 +1,5 @@
 import { FormGroup } from  '@angular/forms';
+import { alerts } from './alerts';
 
 export class functions{
 
@@ -36,7 +37,75 @@ export class functions{
 		}
 	}
 	
+	/*=============================================
+	Función para validar la imagen
+	=============================================*/
 
+	static validateImage(e:any):any{
+
+		return new Promise(resolve => {
+
+			const image = e.target.files[0];
+			
+			/*=============================================
+		    Validamos el formato
+		    =============================================*/
+
+			if(image["type"] !== "image/jpeg" && image["type"] !== "image/png"){
+
+				alerts.basicAlert('error', 'The image must be in JPG or PNG format', 'error');
+
+				return;
+			}
+
+		    /*=============================================
+		    Validamos el tamaño
+		    =============================================*/
+
+			else if (image["size"] > 2000000){
+
+				alerts.basicAlert('error', 'Image must not weigh more than 2MB', 'error');
+
+				return;
+			}
+
+		    /*=============================================
+		    Mostramos la imagen temporal
+		    =============================================*/
+
+			else {
+
+				let data = new FileReader();
+				data.readAsDataURL(image);
+
+				data.onloadend = () => {
+
+					resolve(data.result);
+
+				}
+			}
+		})
+
+	}
+
+	/*=============================================
+	Crear URL
+	=============================================*/
+
+	static createUrl(value:string) {
+
+		value = value.toLocaleLowerCase();
+		value = value.replace(/[ ]/g, "-");
+		value = value.replace(/[á]/g, "a");
+		value = value.replace(/[é]/g, "e");
+		value = value.replace(/[í]/g, "i");
+		value = value.replace(/[ó]/g, "o");
+		value = value.replace(/[ú]/g, "u");
+		value = value.replace(/[ñ]/g, "n");
+		value = value.replace(/[,]/g, "");
+
+		return value;
+	}
 }
 
 
