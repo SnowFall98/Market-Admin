2b81994da6d224c088ef71e9adebc266c0c632b5
SnowFall98 <yordymora98@gmail.com>
2022-05-16 14:25:44 -0500

    Correción en los Pipes

--- src/app/pipes/pipes.module.ts
+++ src/app/pipes/pipes.module.ts
@@ -1,17 +1,16 @@
-import { CommonModule } from "@angular/common";
-import { NgModule } from "@angular/core";
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
 
-// Pipe
-import { JsonToStringPipe } from "./json-to-string.pipe";
+//pipes
+import { JsonToStringPipe } from './json-to-string.pipe';
 
 @NgModule({
-    declarations: [JsonToStringPipe],
-    imports: [
-        CommonModule
-    ],
-    exports: [
-        JsonToStringPipe
-    ]
+  declarations: [JsonToStringPipe],
+  imports: [
+    CommonModule
+  ],
+  exports:[
+	 JsonToStringPipe
+  ]
 })
-
-export class PipesModule {}
+export class PipesModule { }
