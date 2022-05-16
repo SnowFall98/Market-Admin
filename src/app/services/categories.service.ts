113df1f67ea908b4e7ae8f51f9f63b8da61b440e
SnowFall98 <yordymora98@gmail.com>
2022-05-16 14:26:06 -0500

    Nuevas funcioenes categories.services

--- src/app/services/categories.service.ts
+++ src/app/services/categories.service.ts
@@ -8,49 +8,48 @@ import { Icategories } from 'src/app/interface/icategories';
 })
 export class CategoriesService {
 
-  constructor(private http:HttpClient) { }
+	constructor(private http:HttpClient ) { }
 
-  /*=============================================
+	/*=============================================
 	Tomar la data de la colección categorías en Firebase
 	=============================================*/
 
-  getData(){
-    
-    return this.http.get(`${environment.urlFirebase}categories.json`);
+	getData(){
 
-  }
+		return this.http.get(`${environment.urlFirebase}categories.json`);
+	}
 
-  /*=============================================
+	/*=============================================
 	Tomar data filtrada de la colección categorías en Firebase
 	=============================================*/
 
-  getFilterData(orderBy:string, equalTo:string){
+	getFilterData(orderBy:string, equalTo:string){
 
-    return this.http.get(`${environment.urlFirebase}categories.json?orderBy="${orderBy}"&equalTo="${equalTo}"&print=pretty`);
+		return this.http.get(`${environment.urlFirebase}categories.json?orderBy="${orderBy}"&equalTo="${equalTo}"&print=pretty`);
 
-  }
-  
+	}
+
 	/*=============================================
 	Guardar información de la categoría
 	=============================================*/
 
-  postData(data: Icategories){
+	postData(data: Icategories){
 
-    return this.http.post(`${environment.urlFirebase}categories.json`, data);
-    
-  }
+		return this.http.post(`${environment.urlFirebase}categories.json`, data);
 
-  /*=============================================
+	}
+
+	/*=============================================
 	Actualizar información 
 	=============================================*/
 
-  patchData(id:string, data:object){
+	patchData(id:string, data:object){
 
-    return this.http.patch(`${environment.urlFirebase}categories/${id}.json`, data);
-    
-  }
+		return this.http.patch(`${environment.urlFirebase}categories/${id}.json`, data);
 
-  /*=============================================
+	}
+
+	/*=============================================
 	Tomar un item de la data colección categorías en Firebase
 	=============================================*/
 
@@ -58,6 +57,16 @@ export class CategoriesService {
 
 		return this.http.get(`${environment.urlFirebase}categories/${id}.json`);
 		
+	}
+
+	/*=============================================
+	Eliminar categoría
+	=============================================*/
+
+	deleteData(id:string){
+
+		return this.http.delete(`${environment.urlFirebase}categories/${id}.json`);
+
 	}
 
 }
