5a194407483424f7d58c5b4f86322d2efdcb6c3b
SnowFall98 <yordymora98@gmail.com>
2022-05-18 18:07:13 -0500

    Configuración del modulo stores

--- src/app/pages/main-page/stores/stores.module.ts
+++ src/app/pages/main-page/stores/stores.module.ts
@@ -1,5 +1,6 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { ReactiveFormsModule } from '@angular/forms';
 
 //ruta
 import { StoresRoutingModule } from './stores-routing.module';
@@ -7,11 +8,32 @@ import { StoresRoutingModule } from './stores-routing.module';
 //componente
 import { StoresComponent } from './stores.component';
 
+//Angular Material
+import { MatTableModule } from '@angular/material/table';
+import { MatPaginatorModule } from '@angular/material/paginator';
+import { MatSortModule } from '@angular/material/sort';
+import {MatDialogModule} from '@angular/material/dialog';
+import {MatChipsModule} from '@angular/material/chips';
+import {MatIconModule} from '@angular/material/icon';
+
+//pipes
+import { PipesModule } from '../../../pipes/pipes.module';
+import { EditStoresComponent } from './edit-stores/edit-stores.component';
+//import { EditStoresComponent } from './edit-stores/edit-stores.component';
+
 @NgModule({
-  declarations: [StoresComponent],
+  declarations: [StoresComponent, EditStoresComponent],
   imports: [
     CommonModule,
-    StoresRoutingModule
+    StoresRoutingModule,
+    MatTableModule,
+    MatPaginatorModule,
+    MatSortModule,
+    MatDialogModule,
+    ReactiveFormsModule,
+    MatChipsModule,
+    MatIconModule,
+    PipesModule
   ]
 })
 export class StoresModule { }
