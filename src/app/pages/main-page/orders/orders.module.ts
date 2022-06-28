fd43928564a2da58228005e21fb05dd6c9408861
SnowFall98 <yordymora98@gmail.com>
2022-06-28 18:20:22 -0500

    Importacion de modulos para manejar order

--- src/app/pages/main-page/orders/orders.module.ts
+++ src/app/pages/main-page/orders/orders.module.ts
@@ -1,5 +1,6 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { ReactiveFormsModule } from '@angular/forms';
 
 //ruta
 import { OrdersRoutingModule } from './orders-routing.module';
@@ -7,11 +8,31 @@ import { OrdersRoutingModule } from './orders-routing.module';
 //componente
 import { OrdersComponent } from './orders.component';
 
+//Angular Material
+import { MatTableModule } from '@angular/material/table';
+import { MatPaginatorModule } from '@angular/material/paginator';
+import { MatSortModule } from '@angular/material/sort';
+import { MatDialogModule} from '@angular/material/dialog';
+import { MatChipsModule} from '@angular/material/chips';
+import { MatIconModule} from '@angular/material/icon';
+
+//pipes
+import { PipesModule } from 'src/app/pipes/pipes.module';
+import { EditOrdersComponent } from './edit-orders/edit-orders.component';
+
 @NgModule({
-  declarations: [OrdersComponent],
+  declarations: [OrdersComponent, EditOrdersComponent],
   imports: [
     CommonModule,
-    OrdersRoutingModule
+    OrdersRoutingModule,
+    MatTableModule,
+    MatPaginatorModule,
+    MatSortModule,
+    MatDialogModule,
+    ReactiveFormsModule,
+    MatChipsModule,
+    MatIconModule,
+    PipesModule
   ]
 })
 export class OrdersModule { }
