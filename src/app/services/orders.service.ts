e7b7aa2dfa9688e653376aa964be4b71739ab617
SnowFall98 <yordymora98@gmail.com>
2022-06-28 17:21:45 -0500

    Creacion del servicio de orders

---
+++ src/app/services/orders.service.ts
@@ -0,0 +1,21 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { environment } from 'src/environments/environment';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class OrdersService {
+
+  constructor(private http:HttpClient) { }
+
+  /*=============================================
+  Tomar la data de la colección órdenes en Firebase
+  =============================================*/
+
+  getData(){
+
+    return this.http.get(`${environment.urlFirebase}orders.json`);
+    
+  }
+}
