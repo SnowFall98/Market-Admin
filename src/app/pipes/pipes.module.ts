4c0f6c63751c2b9cde49a42af6c9391f9f618714
SnowFall98 <yordymora98@gmail.com>
2022-06-28 13:43:06 -0500

    Nuevos Pipes de enrutamiento

--- src/app/pipes/pipes.module.ts
+++ src/app/pipes/pipes.module.ts
@@ -3,14 +3,18 @@ import { CommonModule } from '@angular/common';
 
 //pipes
 import { JsonToStringPipe } from './json-to-string.pipe';
+import { SeparateObjectsPipe } from './separate-objects.pipe';
+import { UrlSecurePipe } from './url-secure.pipe';
 
 @NgModule({
-  declarations: [JsonToStringPipe],
+  declarations: [JsonToStringPipe, SeparateObjectsPipe, UrlSecurePipe],
   imports: [
     CommonModule
   ],
   exports:[
-	 JsonToStringPipe
+	 JsonToStringPipe,
+   SeparateObjectsPipe,
+   UrlSecurePipe
   ]
 })
 export class PipesModule { }
