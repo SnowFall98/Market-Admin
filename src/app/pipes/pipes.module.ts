98cea0a54017191e8a91ef4ef256aae1190b6551
SnowFall98 <yordymora98@gmail.com>
2022-06-29 18:50:08 -0500

    New Pipe substr, reducir tamaños

--- src/app/pipes/pipes.module.ts
+++ src/app/pipes/pipes.module.ts
@@ -5,16 +5,18 @@ import { CommonModule } from '@angular/common';
 import { JsonToStringPipe } from './json-to-string.pipe';
 import { SeparateObjectsPipe } from './separate-objects.pipe';
 import { UrlSecurePipe } from './url-secure.pipe';
+import { SubstrPipe } from './substr.pipe';
 
 @NgModule({
-  declarations: [JsonToStringPipe, SeparateObjectsPipe, UrlSecurePipe],
+  declarations: [JsonToStringPipe, SeparateObjectsPipe, UrlSecurePipe, SubstrPipe],
   imports: [
     CommonModule
   ],
   exports:[
 	 JsonToStringPipe,
    SeparateObjectsPipe,
-   UrlSecurePipe
+   UrlSecurePipe,
+   SubstrPipe
   ]
 })
 export class PipesModule { }
