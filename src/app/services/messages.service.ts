13a976304c4efe112c21d16f3ca3f496e443dbdd
SnowFall98 <yordymora98@gmail.com>
2022-06-29 18:49:48 -0500

    API message services

--- src/app/services/messages.service.ts
+++ src/app/services/messages.service.ts
@@ -1,9 +1,40 @@
 import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { environment } from 'src/environments/environment';
 
 @Injectable({
   providedIn: 'root'
 })
 export class MessagesService {
 
-  constructor() { }
+  constructor(private http: HttpClient) { }
+
+  /*=============================================
+  Tomar la data de la colección Mensajes en Firebase
+  =============================================*/
+
+  getData(){
+
+    return this.http.get(`${environment.urlFirebase}messages.json`);
+  } 
+
+  /*=============================================
+  Tomar un item de la data colección Mensajes en Firebase
+  =============================================*/
+
+  getItem(id: string) {
+
+    return this.http.get(`${environment.urlFirebase}messages/${id}.json`);
+    
+  } 
+
+  /*=============================================
+  Actualizar información 
+  =============================================*/
+
+  patchData(id:string, data:object){
+
+    return this.http.patch(`${environment.urlFirebase}messages/${id}.json`, data);
+
+  }
 }
