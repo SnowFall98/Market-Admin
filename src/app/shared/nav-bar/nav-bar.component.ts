cdc5b03bcf772a630640101faa78224cb036e8e7
SnowFall98 <yordymora98@gmail.com>
2022-07-08 12:24:50 -0500

    Front, Back del Navbar

--- src/app/shared/nav-bar/nav-bar.component.ts
+++ src/app/shared/nav-bar/nav-bar.component.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { MessagesService } from 'src/app/services/messages.service';
+import { DisputesService } from 'src/app/services/disputes.service';
+import { environment } from 'src/environments/environment';
 
 @Component({
   selector: 'app-nav-bar',
@@ -7,9 +11,52 @@ import { Component, OnInit } from '@angular/core';
 })
 export class NavBarComponent implements OnInit {
 
-  constructor() { }
+  /*=============================================
+  Variable global para definir mensajes sin responder
+  =============================================*/
+  messages = 0;
+
+  /*=============================================
+  Variable global para definir disputa sin responder
+  =============================================*/
+  disputes = 0;
+
+  constructor(private router: Router, public messagesService: MessagesService, public disputesService: DisputesService) { }
 
   ngOnInit(): void {
+
+    this.getMessages();
+    this.getDisputes();
+  }
+
+  /*=============================================
+  Mensajes sin responder
+  =============================================*/
+
+  getMessages(){
+
+    this.messagesService.getFilterData("receiver", environment.nameStore)
+    .subscribe()
+  }
+
+  /*=============================================
+  Disputas sin responder
+  =============================================*/
+
+  getDisputes(){
+
+    this.disputesService.getFilterData("receiver", environment.nameStore)
+    .subscribe();
+  }
+
+  /*=============================================
+  Función de salida del sistema
+  =============================================*/
+  logout(){
+
+    localStorage.removeItem('token');
+    localStorage.removeItem('refreshToken');
+    this.router.navigateByUrl("/login");
   }
 
 }
