9b960af4fc46d42a76bb8f799a7f616b9948b039
SnowFall98 <yordymora98@gmail.com>
2022-07-08 12:23:34 -0500

    Filtrar datos para funciones del Navbar

--- src/app/services/messages.service.ts
+++ src/app/services/messages.service.ts
@@ -1,11 +1,14 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { environment } from 'src/environments/environment';
+import { map } from 'rxjs/operators';
 
 @Injectable({
   providedIn: 'root'
 })
 export class MessagesService {
+
+  messages:number = 0;
 
   constructor(private http: HttpClient) { }
 
@@ -35,6 +38,35 @@ export class MessagesService {
   patchData(id:string, data:object){
 
     return this.http.patch(`${environment.urlFirebase}messages/${id}.json`, data);
+
+  }
+
+  /*=============================================
+  Tomar data filtrada de la colección mensajes en Firebase
+  =============================================*/
+
+  getFilterData(orderBy:string, equalTo:string){
+
+    return this.http.get(`${environment.urlFirebase}messages.json?orderBy="${orderBy}"&equalTo="${equalTo}"&print=pretty`).pipe(
+      map((resp:any)=>{
+
+        this.messages = 0;
+
+        Object.keys(resp).map( (a) => {
+
+          if(resp[a].answer == undefined){
+
+            this.messages++
+
+          }
+      
+        });
+
+        return resp;
+
+      })
+
+    );
 
   }
 }
